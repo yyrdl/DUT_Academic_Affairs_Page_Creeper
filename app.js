2233e121189c18a631c2027be0e72210c911794d
yyrdl <yyrdl1215@163.com>
2015-10-14 21:05:11 +0800

    f

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-var http=require("http"),tools=require("./lib/tool"),service=require("./lib/service"),handleIncomming=function(e,n){tools.getIncomingDataFromUrl(e).then(function(e){return tools.auth(e.key).then(function(){return obj={username:e.username,password:e.password,type:e.type}})}).then(function(e){return service.serve(e)}).then(function(e){tools.sendOKRes(n,e)})["catch"](function(e){console.error(e.stack),tools.sendError(n,e)})},server=http.createServer(handleIncomming);server.listen(3e3,function(){console.log("Server running at 3000")});
+var http = require("http"), tools = require("./lib/tool"), service = require("./lib/service"), handleIncomming = function (e, n) {
+	tools.getIncomingDataFromUrl(e).then(function (e) {
+		return tools.auth(e.key).then(function () {
+			return obj = {
+				username : e.username,
+				password : e.password,
+				type : e.type
+			}
+		})
+	}).then(function (e) {
+		return service.serve(e)
+	}).then(function (e) {
+		tools.sendOKRes(n, e)
+	})["catch"](function (e) {
+		console.error(e.stack),
+		tools.sendError(n, e)
+	})
+}, server = http.createServer(handleIncomming);
+server.listen(3e3, function () {
+	console.log("Server running at 3000")
+});
